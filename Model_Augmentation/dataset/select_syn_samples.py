f98f9642ca9638613783ac1dd2f9155a5d261347
Stefano Bini <sbini@unisa.it>
2022-06-18 02:37:33 +0200

    fix dataset creation

--- Model_Augmentation/dataset/select_syn_samples.py
+++ Model_Augmentation/dataset/select_syn_samples.py
@@ -7,7 +7,7 @@ from pydub import AudioSegment
 
 
 SRC_SYN_DATA_PATH = 'Dataset_synth/'
-DST_CMD_DATA_PATH = 'FELICE_demo7_extended/commands/'
+DST_CMD_DATA_PATH = 'FELICE_demo7_extended/synthetics/'
 DST_RJT_DATA_PATH = 'FELICE_demo7_extended/rejects/'
 
 
@@ -41,66 +41,69 @@ def create_folder(base_path, folder):
 
 dataset_iterator = tqdm(os.listdir(SRC_SYN_DATA_PATH))
 for platform in dataset_iterator:
-    ptf_syn_path = os.path.join(SRC_SYN_DATA_PATH, platform)
+    if '.' not in platform:
+        ptf_syn_path = os.path.join(SRC_SYN_DATA_PATH, platform)
 
-    # create platform folder in dataset
-    ptf_cmd_path = create_folder(DST_CMD_DATA_PATH, platform)
-    ptf_rjt_path = create_folder(DST_RJT_DATA_PATH, platform)
+        # create platform folder in dataset
+        ptf_cmd_path = create_folder(DST_CMD_DATA_PATH, platform)
+        ptf_rjt_path = create_folder(DST_RJT_DATA_PATH, platform)
 
-    for speaker in os.listdir(ptf_syn_path):
-        spk_syn_path = os.path.join(ptf_syn_path, speaker)
+        for speaker in os.listdir(ptf_syn_path):
+            if '.' not in speaker:
+                spk_syn_path = os.path.join(ptf_syn_path, speaker)
 
-        # create speaker folder in dataset
-        spk_cmd_path = create_folder(ptf_cmd_path, speaker)
-        spk_cmd_path_ext = create_folder(ptf_cmd_path, speaker+'_take')
-        spk_rjt_path = create_folder(ptf_rjt_path, speaker)
+                # create speaker folder in dataset
+                spk_cmd_path = create_folder(ptf_cmd_path, speaker)
+                spk_cmd_path_ext = create_folder(ptf_cmd_path, speaker+'_take')
+                #spk_rjt_path = create_folder(ptf_rjt_path, speaker)
 
-        for language in os.listdir(spk_syn_path):
-            lang_syn_path = os.path.join(spk_syn_path, language)
-            
-            # create language folder in dataset
-            lang_cmd_path = create_folder(spk_cmd_path, language)
-            lang_cmd_path_ext = create_folder(spk_cmd_path_ext, language)
-            lang_rjt_path = create_folder(spk_rjt_path, language)
+                for language in os.listdir(spk_syn_path):
+                    if '.' not in language:
+                        dataset_iterator.set_description('Analizing {}/{}/{}'.format(platform, speaker, language))
+                        
+                        lang_syn_path = os.path.join(spk_syn_path, language)
+                        
+                        # create language folder in dataset
+                        lang_cmd_path = create_folder(spk_cmd_path, language)
+                        lang_cmd_path_ext = create_folder(spk_cmd_path_ext, language)
+                        lang_rjt_path = create_folder(ptf_rjt_path, language)
 
-            # for "start" command, taht is the same between italian and english commands
-            other_lang = 'ita' if language == 'eng' else 'eng'
-            other_lang_cmd_path = create_folder(spk_cmd_path_ext, other_lang)
-            
-            for filee in os.listdir(lang_syn_path):
-                file_syn_path = os.path.join(lang_syn_path, filee)
-                
-                if '.wav' in file_syn_path:
-                    regex = '[0-9]{1,2}\.wav$'
-                    cmd = int(re.search(pattern=regex, string=file_syn_path).group().split('.')[0])
-                    if cmd in CMDS_LIST:
-                        file_cmd_path = os.path.join(lang_cmd_path, filee.replace(str(cmd), str(CMDS_DICT[cmd])))
-                        shutil.copyfile(file_syn_path, file_cmd_path)
-                        if cmd == 18:
-                            file_cmd_path = os.path.join(other_lang_cmd_path, filee.replace(str(cmd), str(CMDS_DICT[cmd])))
-                            shutil.copyfile(file_syn_path, file_cmd_path)
-                    elif cmd in CMDS_EXT_LIST:
-                        file_cmd_path_ext = os.path.join(lang_cmd_path_ext, filee.replace(str(cmd), str(CMDS_EXT_DICT[cmd])))
-                        shutil.copyfile(file_syn_path, file_cmd_path_ext)
-                    else:
-                        file_rjt_path_ext = os.path.join(lang_rjt_path, filee)
-                        shutil.copyfile(file_syn_path, file_rjt_path_ext)
-                
-                elif '.mp3' in file_syn_path:
-                    file_wav = AudioSegment.from_ogg(file_syn_path) # from .ogg to .wav
-                    regex = '[0-9]{1,2}\.mp3$'
-                    cmd = int(re.search(pattern=regex, string=file_syn_path).group().split('.')[0])
-                    if cmd in CMDS_LIST:
-                        file_cmd_path = os.path.join(lang_cmd_path, filee.replace(str(cmd), str(CMDS_DICT[cmd])))
-                        file_wav.export(file_cmd_path, format='wav')
-                        if cmd == 18:
-                            file_cmd_path = os.path.join(other_lang_cmd_path, filee.replace(str(cmd), str(CMDS_DICT[cmd])))
-                            file_wav.export(file_cmd_path, format='wav')
-                    elif cmd in CMDS_EXT_LIST:
-                        file_cmd_path_ext = os.path.join(lang_cmd_path_ext, filee.replace(str(cmd), str(CMDS_EXT_DICT[cmd])))
-                        file_wav.export(file_cmd_path_ext, format='wav')
-                    else:
-                        file_rjt_path_ext = os.path.join(lang_rjt_path, filee)
-                        file_wav.export(file_rjt_path_ext, format='wav')
-    
-    dataset_iterator.set_description('Analizing')
+                        # for "start" command, taht is the same between italian and english commands
+                        other_lang = 'ita' if language == 'eng' else 'eng'
+                        other_lang_cmd_path = create_folder(spk_cmd_path_ext, other_lang)
+                        
+                        for filee in os.listdir(lang_syn_path):
+                            file_syn_path = os.path.join(lang_syn_path, filee)
+                            
+                            if '.wav' in file_syn_path:
+                                regex = '[0-9]{1,2}\.wav$'
+                                cmd = int(re.search(pattern=regex, string=file_syn_path).group().split('.')[0])
+                                if cmd in CMDS_LIST:
+                                    file_cmd_path = os.path.join(lang_cmd_path, filee.replace(str(cmd), str(CMDS_DICT[cmd])))
+                                    shutil.copyfile(file_syn_path, file_cmd_path)
+                                    if cmd == 18:
+                                        file_cmd_path = os.path.join(other_lang_cmd_path, filee.replace(str(cmd), str(CMDS_DICT[cmd])))
+                                        shutil.copyfile(file_syn_path, file_cmd_path)
+                                elif cmd in CMDS_EXT_LIST:
+                                    file_cmd_path_ext = os.path.join(lang_cmd_path_ext, filee.replace(str(cmd), str(CMDS_EXT_DICT[cmd])))
+                                    shutil.copyfile(file_syn_path, file_cmd_path_ext)
+                                else:
+                                    file_rjt_path_ext = os.path.join(lang_rjt_path, filee)
+                                    shutil.copyfile(file_syn_path, file_rjt_path_ext)
+                            
+                            elif '.mp3' in file_syn_path:
+                                file_wav = AudioSegment.from_mp3(file_syn_path) # from .mp3 to .wav
+                                regex = '[0-9]{1,2}\.mp3$'
+                                cmd = int(re.search(pattern=regex, string=file_syn_path).group().split('.')[0])
+                                if cmd in CMDS_LIST:
+                                    file_cmd_path = os.path.join(lang_cmd_path, filee.replace(str(cmd), str(CMDS_DICT[cmd])))
+                                    file_wav.export(file_cmd_path, format='wav')
+                                    if cmd == 18:
+                                        file_cmd_path = os.path.join(other_lang_cmd_path, filee.replace(str(cmd), str(CMDS_DICT[cmd])))
+                                        file_wav.export(file_cmd_path, format='wav')
+                                elif cmd in CMDS_EXT_LIST:
+                                    file_cmd_path_ext = os.path.join(lang_cmd_path_ext, filee.replace(str(cmd), str(CMDS_EXT_DICT[cmd])))
+                                    file_wav.export(file_cmd_path_ext, format='wav')
+                                else:
+                                    file_rjt_path_ext = os.path.join(lang_rjt_path, filee)
+                                    file_wav.export(file_rjt_path_ext, format='wav')
