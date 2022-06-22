a56576ea8f119c3f96570c503b797af6f672bda9
Stefano Bini <sbini@unisa.it>
2022-06-22 10:46:50 +0200

    from dldev6

--- Model_Augmentation/dataset/include_synthetics.py
+++ Model_Augmentation/dataset/include_synthetics.py
@@ -83,16 +83,16 @@ for platform in dataset_iterator:
                                 regex = '[0-9]{1,2}\.mp3$'
                                 cmd = int(re.search(pattern=regex, string=file_syn_path).group().split('.')[0])
                                 if cmd in DEMO7_CMDS_LIST:
-                                    file_cmd_path = os.path.join(lang_cmd_path, filee.replace(str(cmd), str(DEMO7_CMDS_DICT[cmd])))
+                                    file_cmd_path = os.path.join(lang_cmd_path, filee.replace(str(cmd), str(DEMO7_CMDS_DICT[cmd])).replace('.mp3', '.wav'))
                                     file_wav.export(file_cmd_path, format='wav')
                                     if cmd == 18:
-                                        file_cmd_path = os.path.join(other_lang_cmd_path, filee.replace(str(cmd), str(DEMO7_CMDS_DICT[cmd])))
+                                        file_cmd_path = os.path.join(other_lang_cmd_path, filee.replace(str(cmd), str(DEMO7_CMDS_DICT[cmd])).replace('.mp3', '.wav'))
                                         file_wav.export(file_cmd_path, format='wav')
                                 elif cmd in DEMO7_CMDS_EXT_LIST:
-                                    file_cmd_path_ext = os.path.join(lang_cmd_path_ext, filee.replace(str(cmd), str(DEMO7_CMDS_DICT_EXT[cmd])))
+                                    file_cmd_path_ext = os.path.join(lang_cmd_path_ext, filee.replace(str(cmd), str(DEMO7_CMDS_DICT_EXT[cmd])).replace('.mp3', '.wav'))
                                     file_wav.export(file_cmd_path_ext, format='wav')
                                 else:
-                                    file_rjt_path_ext = os.path.join(lang_rjt_path, filee)
+                                    file_rjt_path_ext = os.path.join(lang_rjt_path, filee.replace('.mp3', '.wav'))
                                     file_wav.export(file_rjt_path_ext, format='wav')
 '''
 
@@ -138,5 +138,5 @@ for platform in dataset_iterator:
                                 regex = '[0-9]{1,2}\.mp3$'
                                 cmd = int(re.search(pattern=regex, string=file_syn_path).group().split('.')[0])
                                 if cmd in DEMO3_CMDS_LIST:
-                                    file_cmd_path = os.path.join(lang_cmd_path, filee.replace(str(cmd), str(DEMO3_CMDS_DICT[cmd])))
+                                    file_cmd_path = os.path.join(lang_cmd_path, filee.replace(str(cmd), str(DEMO3_CMDS_DICT[cmd]).replace('.mp3', '.wav')))
                                     file_wav.export(file_cmd_path, format='wav')
